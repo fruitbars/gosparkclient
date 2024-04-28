1af49a01741de6ce1b0334bb19d22d62334cfbed
fruitbars <huiyanghu@gmail.com>
2024-04-29 00:27:42 +0800

    test main.go add test func

--- test/main.go
+++ test/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"gosparkclient"
+	"github.com/fruitbars/gosparkclient"
 	"log"
 )
 
-func main() {
+func testDefeult() {
 	client := gosparkclient.NewSparkClient()
 
 	r, sid, err := client.SparkChatSimple("你好")
@@ -27,4 +27,32 @@ func main() {
 	}
 
 	log.Println(sid, r)
+}
+
+func testEnv() {
+	client := gosparkclient.NewSparkClientWithEnv("dev_v3.env")
+
+	r, sid, err := client.SparkChatSimple("你好")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(sid, r)
+
+	r, sid, err = client.SparkChatWithCallback(gosparkclient.SparkChatRequest{
+		Prompt: "你好",
+	}, func(response gosparkclient.SparkAPIResponse) {
+		if len(response.Payload.Choices.Text) > 0 {
+			log.Println(response.Header.Sid, response.Payload.Choices.Text[0].Content)
+		}
+
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(sid, r)
+}
+
+func main() {
+
 }
